56bfd111272affeffdc8d42cdcfe7fdb066db6ae
Ahnaf Sindid <sindma@farmingdale.edu>
2025-04-08 12:53:55 -0400

    fixed imageview and css error

--- javasample/src/main/java/murray/javasample/HelloApplication.java
+++ javasample/src/main/java/murray/javasample/HelloApplication.java
@@ -3,7 +3,6 @@ package murray.javasample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +14,6 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/murray/javasample/login-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("/murray/javasample/Login-Screen.css").toExternalForm());
-        new Image(getClass().getResource("/murray.javasample/login_ui.jpg").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
--- javasample/src/main/java/murray/javasample/LoginController.java
+++ javasample/src/main/java/murray/javasample/LoginController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class LoginController {
 
